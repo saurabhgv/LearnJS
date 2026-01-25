eeef466be384f269415abf31dab65ac3a383ffc6
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-25 19:28:25 +0530

    added program1.js

---
+++ program1.js
@@ -0,0 +1,24 @@
+// Problem 1 — Very Basic (Variables & Output)
+// Task
+
+// Write JavaScript code that:
+
+// Stores a user’s name in a variable
+
+// Stores a user’s age in another variable
+
+// Prints this message to the console:
+
+// Hello, my name is <name> and I am <age> years old.
+// Rules (important)
+
+// Use const where appropriate
+
+// Use clear variable names
+
+// Use only JavaScript (console output is fine)
+
+// No shortcuts — write clean, readable code
+
+// Example output (for name = Alex, age = 25):
+// Hello, my name is Alex and I am 25 years old.
