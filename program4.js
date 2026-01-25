984f073ff22eafbf31aec7859e849d328a254c2a
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-26 01:17:19 +0530

    adding new file

---
+++ program4.js
@@ -0,0 +1,27 @@
+// 🟢 Problem 4 — Multiple Conditions (Step Up)
+// Task
+
+// Create a variable score
+
+// If score is 90 or above → print "Grade A"
+
+// Else if score is 70 or above → print "Grade B"
+
+// Else → print "Grade C"
+
+// Rules
+
+// Use if / else if / else
+
+// Use correct comparison logic
+
+// Clean, readable code
+
+const score=93
+if (score>=90){
+    console.log("Grade A")
+}else if(score>=70){
+    console.log("Grade B")
+}else{
+    console.log("Grade C")
+}
