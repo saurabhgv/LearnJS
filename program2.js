08551c98e2a6683043caacf5f99484cad05a722a
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-25 20:09:52 +0530

    add new file

---
+++ program2.js
@@ -0,0 +1,20 @@
+// 🟢 Problem 2 — Variables & Reassignment (Do This Now)
+// Task
+
+// Create a variable called score with value 10
+
+// Increase the score by 5
+
+// Print the final score
+
+// Rules
+
+// Use let or const correctly
+
+// Clean, readable code
+
+// No unnecessary console logs
+
+let score =10
+score=score+5
+console.log(score)
