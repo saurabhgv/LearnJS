cd5043c1fd5927852bc59548b920f3148b593866
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-25 22:27:27 +0530

    add new file

---
+++ program3.js
@@ -0,0 +1,30 @@
+// 🟢 Problem 3 — Conditions (if / else)
+// Task
+
+// Create a variable called temperature
+
+// If temperature is greater than 30, print:
+
+// It's hot
+
+
+// Otherwise, print:
+
+// It's not hot
+
+// Rules
+
+// Use const or let correctly
+
+// Use if / else
+
+// Clean formatting, readable code
+
+
+let temperature=25
+if(temperature>30){
+    console.log("It's hot")
+}
+else{
+    console.log("It's not hot")
+}
