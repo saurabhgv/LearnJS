88463d3f1f0e6f8a76f10423129c5adbbb833733
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-26 23:07:49 +0530

    adding file

---
+++ program10.js
@@ -0,0 +1,25 @@
+// 🟢 Problem 10 — Arrays + Loop (Do This Now)
+// Task
+
+// Write JavaScript code that:
+
+// Creates an array numbers with [1, 2, 3, 4, 5]
+
+// Loops through the array
+
+// Prints each number multiplied by 2
+
+// Do not modify the original array
+
+// Rules
+
+// Use a for loop
+
+// Clean variable names
+
+// No shortcuts yet
+
+const numbers=[1, 2, 3, 4, 5]
+for (let i=0;i<numbers.length;i++){
+    console.log(numbers[i]*2)
+}
