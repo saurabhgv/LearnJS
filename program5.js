a5d8814a8fadc80f25901f06db00ef0405d01b23
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-26 21:59:49 +0530

    adding file

---
+++ program5.js
@@ -0,0 +1,20 @@
+// 🟢 Problem 5 — Loops (Next Core Skill)
+// Task
+
+// Write JavaScript code that:
+
+// Prints numbers from 1 to 5
+
+// Each number should be printed on a new line
+
+// Rules
+
+// Use a for loop
+
+// Clean, readable code
+
+// Correct variable scoping
+
+for(let i=1;i<=5;i++){
+    console.log(i)
+}
