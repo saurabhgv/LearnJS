5a18b2b6a2343d24ead3fd2b90cc7579dde423d3
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-27 00:28:37 +0530

    adding file

---
+++ program13.js
@@ -0,0 +1,24 @@
+// 🟢 Problem 13 — Objects + Update (Important Concept)
+// Task
+
+// Write JavaScript code that:
+
+// Creates an object person with name and age
+
+// Updates the age by increasing it by 1
+
+// Prints the updated object
+
+// Rules
+
+// Use const for the object
+
+// Mutate the object correctly
+
+// Clean, readable code
+const person={
+    name:"saurabh",
+    age:25
+}
+person.age=person.age+1
+console.log(person)
