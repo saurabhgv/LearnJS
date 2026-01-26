0f95ac2b86202d716bba6419ec1c2e85e3869ab6
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-26 23:31:48 +0530

    adding file

---
+++ program12.js
@@ -0,0 +1,29 @@
+// 🟢 Problem 12 — Objects (Do This Now)
+// Task
+
+// Write JavaScript code that:
+
+// Creates an object called person with:
+
+// name
+
+// age
+
+// Prints the person’s name
+
+// Prints the person’s age
+
+// Rules
+
+// Use const
+
+// Use dot notation
+
+// Clean, readable code
+
+const person={
+    name:"Saurabh",
+    age:25
+}
+console.log(person.name)
+console.log(person.age)
