5bef4c027d1b67dcd937cf2447edba34db5b47ea
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-26 22:50:16 +0530

    adding file

---
+++ program8.js
@@ -0,0 +1,26 @@
+// 🟢 Problem 8 — Functions with Return (Very Important)
+// Task
+
+// Write a function called add that:
+
+// Takes two parameters: a and b
+
+// Returns their sum
+
+// Store the returned value in a variable
+
+// Print the result
+
+// Rules
+
+// Use return (not console.log inside the function)
+
+// Clean naming
+
+// Clear separation of logic and output
+
+function add(a,b){
+    return a+b;
+}
+const sum=add(10,20)
+console.log(sum)
