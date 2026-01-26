01f473d962b0e7d35531b26a8c788d0c469f274b
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-26 23:00:34 +0530

    adding file

---
+++ program9.js
@@ -0,0 +1,22 @@
+// 🟢 Problem 9 — Arrays (Next Big Concept)
+// Task
+
+// Write JavaScript code that:
+
+// Creates an array called numbers with values [1, 2, 3, 4, 5]
+
+// Prints the length of the array
+
+// Prints the last element of the array
+
+// Rules
+
+// Use const for the array
+
+// No hardcoding indexes for “last element”
+
+// Clean, readable code
+
+const numbers=[1, 2, 3, 4, 5]
+console.log(numbers.length)
+console.log(numbers[numbers.length-1])
