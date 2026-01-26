4532390dd6711c4d622ae9c11f26a238233e9a26
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-01-26 22:16:03 +0530

    program7.js

---
+++ program7.js
@@ -0,0 +1,27 @@
+// 🟢 Problem 7 — Functions (Do This Now)
+// Task
+
+// Write JavaScript code that:
+
+// Declares a function named greet
+
+// The function takes one parameter: name
+
+// Inside the function, print:
+
+// Hello, <name>!
+
+
+// Call the function using your name
+
+// Rules
+
+// Use a function declaration (not arrow function yet)
+
+// No global variables inside the function
+
+// Clean, readable code
+function greet(name){
+    console.log(`Hello, ${name}`);
+}
+greet("saurabh")
