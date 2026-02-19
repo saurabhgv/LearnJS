5efdb1af534c5ba16665c9c21b97a8f081f9dfd8
saurabhgaikwad <saurabhgaikwad7027@gmail.com>
2026-02-20 04:27:02 +0530

    adding file

--- get.js
+++ get.js
@@ -1,21 +1,54 @@
 class Node {
-    constructor(value) {
+    constructor(value){
         this.value = value;
         this.next = null;
     }
 }
-
+ 
 class LinkedList {
     constructor(value) {
         const newNode = new Node(value);
         this.head = newNode;
-        this.tail = newNode;
+        this.tail = this.head;
         this.length = 1;
     }
 
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
+    }
+
+    getTail() {
+        if (this.tail === null) {
+            console.log("Tail: null");
+        } else {
+            console.log("Tail: " + this.tail.value);
+        }
+    }
+
+    getLength() {
+        console.log("Length: " + this.length);
+    }
+
+    makeEmpty() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+ 
     push(value) {
         const newNode = new Node(value);
-
         if (!this.head) {
             this.head = newNode;
             this.tail = newNode;
@@ -23,26 +56,77 @@ class LinkedList {
             this.tail.next = newNode;
             this.tail = newNode;
         }
-
         this.length++;
         return this;
     }
-
-    get(index) {
-        if (index < 0 || index >= this.length) return undefined;
-
+ 
+    pop() {
+        if (this.length === 0) return undefined;
         let temp = this.head;
-
-        for (let i = 0; i < index; i++) {
+        let pre = this.head;
+        while (temp.next) {
+            pre = temp;
             temp = temp.next;
         }
-
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
         return temp;
     }
+ 
+    unshift(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+ 
+    shift() {
+        if (this.length === 0) return undefined;
+        let temp = this.head;
+        this.head = this.head.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        temp.next = null;
+        return temp;
+    }
+ 
+    get(index){
+        if(index<0 || index>=this.length) return null
+        let temp=this.head
+        for(let i=0;i<index;i++){
+            temp=temp.next
+        }
+        return temp
+    }
+
 }
-let myObj=new LinkedList(50)
-myObj.push(20)
-myObj.push(30)
-myObj.push(40)
-console.log(myObj.get(2).value)
-// console.log(myObj)
+ 
+
+
+let myLinkedList = new LinkedList(0);
+myLinkedList.push(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+
+console.log(myLinkedList.get(3).value);
+
+ 
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    3
+
+*/
